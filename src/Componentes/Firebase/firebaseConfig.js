56a503a9cf22c869db25aa29da6acee5ea98ceff
Leo Contreras <leonardoaguie@gmail.com>
2023-07-19 14:28:22 -0700

    nueva base de datos

--- src/Componentes/Firebase/firebaseConfig.js
+++ src/Componentes/Firebase/firebaseConfig.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCxV2kEfGd0HNa3k7clnIve89pC0GmFVEg",
-    authDomain: "turnero-adbc.firebaseapp.com",
-    projectId: "turnero-adbc",
-    storageBucket: "turnero-adbc.appspot.com",
-    messagingSenderId: "306996940648",
-    appId: "1:306996940648:web:788d67244ccfc6f88a50dd",
-    measurementId: "G-6EJ4RTE51Q"
+    apiKey: "AIzaSyCPKHwMVftCUJfHHPEUFI8vQ0Iw8XN22LI",
+    authDomain: "turnero-a9c0e.firebaseapp.com",
+    projectId: "turnero-a9c0e",
+    storageBucket: "turnero-a9c0e.appspot.com",
+    messagingSenderId: "350137652889",
+    appId: "1:350137652889:web:fd4f2d7eab7d9499c90a08",
+    measurementId: "G-N8NC2PBTGZ"
 };
 
 // Initialize Firebase
